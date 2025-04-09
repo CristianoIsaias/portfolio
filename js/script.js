b2b499545e7a8ee8c073669737bb7897074c2785
cristiano isaias <crisano79@gmail.com>
2025-04-08 21:33:45 -0300

    Revisão um

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// // Ao passar o mouse 
+//  unction mouseover(){
+//    let cor = document.getElementById("indice3");
+//    cor.style.backgroundColor = "#eae8ea";
+   
+//  }//Ao retirar o mouse
+//  unction mouseout(){
+//     let cor = document.getElementById("indice3")
+//     cor.style.backgroundColor = "#eae8ea";
+    
+//  }
+
